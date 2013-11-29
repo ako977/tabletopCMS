ed82a576e704fa3c9cffe86a321058db2961eb22
Alex Osei-Gyau <aoseigyau@mcmillanagency.com>
2013-11-29 12:33:32 -0500

    implementing core structure

---
+++ example/inc/js/config.js
@@ -0,0 +1,8 @@
+/**
+ * This global configuration object is used by /tabletopcms/modules/website.js to initialize the features of the site you would like to have.
+ * @type {{module_paths: Array, module_names: Array}}
+ */
+var TABLETOPCMS_CONFIG = {
+    module_paths: ['../lib/tabletopcms/modules/google_spreadsheet_cms.js'],
+    module_names: ['tabletopcms.google_spreadsheet_cms']
+};
