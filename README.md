5ac1efe6cece84479f2b662b0a5467463214afae
Alex Osei-Gyau <aoseigyau@mcmillanagency.com>
2013-11-29 17:32:35 -0500

    added jquery for now, may remove dependency later

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 tabletopCMS
 ===========
 
-AngularJS driven basic content management of your website.js's text content. Content reflected is edited in Google Spreadsheet (with tabletopjs library).
+AngularJS driven basic content management of your website's text content. Content reflected is edited in Google Spreadsheet (with tabletopjs library).
+
+## tabletopCMS aims to achieve these goals ##
+
+- SEO compliant (site pages and content can be read by search engine bots, even though site runs on Javascript)
+- Easy implementation
+- Flexible configuration
+- Translations support
+- Browser support for IE 8 or higher
--- example/inc/js/config.js
+++ example/inc/js/config.js
@@ -1,8 +1,10 @@
+
 /**
- * This global configuration object is used by /tabletopcms/modules/website.js to initialize the features of the site you would like to have.
- * @type {{module_paths: Array, module_names: Array}}
+ * This global configuration object is used by /tabletopcms/app.js to initialize the features of the site you would like to have.
+ * @type {{module_paths: Array, module_names: Array, sitemap_id: string}}
  */
 var TABLETOPCMS_CONFIG = {
-    module_paths: ['../lib/tabletopcms/modules/google_spreadsheet_cms.js'],
-    module_names: ['tabletopcms.google_spreadsheet_cms']
+    module_paths: ['../lib/tabletopcms/modules/google_spreadsheet_cms/google_spreadsheet_cms.js'],
+    module_names: ['tabletopcms.google_spreadsheet_cms'],
+    sitemap_id: 'tabletop-sitemap'
 };
